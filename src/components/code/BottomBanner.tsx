be1d491035b53422977bedd6c50f051c64d44d2b
eeinhaus17 <eeinhaus17@gmail.com>
2024-11-16 09:18:43 -0500

    more banner improvements

--- src/components/code/BottomBanner.tsx
+++ src/components/code/BottomBanner.tsx
@@ -21,12 +21,13 @@ export default function BottomBanner() {
   };
 
   return (
-    <div className="absolute bottom-0 left-0 bg-slate-50 bg-opacity-10 w-full flex justify-center items-center">
+    <div className="absolute bottom-0 left-0 bg-blue-900 bg-opacity-50 w-full flex justify-center items-center">
       <div className="p-4 text-center">
         <p className="text-2xl mb-2">
-          {getLanguageLabel()} | {currentSnippet?.description}
+          <span className="font-bold">{getLanguageLabel()}</span> |{' '}
+          {currentSnippet?.description}
         </p>
-        <p className="text-xl text-gray-400">
+        <p className="text-md text-gray-400">
           Correctly type the first character to begin.
         </p>
       </div>
